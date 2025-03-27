056880d8d500d654d0658c7831a13effb9b8e7e8
Durga-velu <durgadevi134babu@gmail.com>
2025-03-27 13:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+let firstName=document.getElementById("fNAME")
+let lastName=document.getElementById("lastNAME")
+let phoneNumber=document.getElementById("phoneNum")
+let email=document.getElementById("emailId")
+
+document.getElementById("submit").addEventListener("click",()=>{
+
+
+alert(`First Name: ${firstName.value} Last Name: ${lastName.value} Phone Number: ${phoneNumber.value} Email ID: ${email.value}`)
+
+
+})
